ac449366e490900f0dc250b40b8ae5a3cdef23bc
deni <deni@debian>
2022-12-05 11:23:04 -0600

    started adding cards

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,5 +1,24 @@
 <template>
-  <q-page class="constrain q-pa-md"> Home </q-page>
+  <q-page class="constrain q-pa-md">
+    <q-card class="card-post" flat bordered>
+      <q-item>
+        <q-item-section avatar>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label class="text-bold">MaliciousQ</q-item-label>
+          <q-item-label caption> Moscow, Russia </q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-separator />
+
+      <q-img src="https://cdn.quasar.dev/img/parallax2.jpg" />
+    </q-card>
+  </q-page>
 </template>
 
 <script>
@@ -7,3 +26,10 @@ export default {
   name: "PageHome",
 };
 </script>
+
+<style lang="sass">
+
+.card-post
+  .q-img
+    min-height: 200px
+</style>
