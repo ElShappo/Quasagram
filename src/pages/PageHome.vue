1e1dfeea929951a099888080ea130e551ff31b68
deni <deni@debian>
2022-12-05 13:59:45 -0600

    added profile block

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,34 +1,48 @@
 <template>
   <q-page class="constrain q-pa-md">
-    <q-card
-      v-for="post in posts"
-      :key="post.id"
-      class="card-post q-mb-md"
-      flat
-      bordered
-    >
-      <q-item>
-        <q-item-section avatar>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
-          </q-avatar>
-        </q-item-section>
+    <div class="row q-col-gutter-lg">
+      <div class="col-12 col-sm-8">
+        <q-card
+          v-for="post in posts"
+          :key="post.id"
+          class="card-post q-mb-md"
+          flat
+          bordered
+        >
+          <q-item>
+            <q-item-section avatar>
+              <q-avatar>
+                <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+              </q-avatar>
+            </q-item-section>
+            <q-item-section>
+              <q-item-label class="text-bold">MaliciousQ</q-item-label>
+              <q-item-label caption> {{ post.location }} </q-item-label>
+            </q-item-section>
+          </q-item>
+          <q-separator />
+          <q-img :src="post.imageUrl" />
+          <q-card-section>
+            <div>{{ post.caption }}</div>
+            <div class="text-caption text-grey">{{ fancyTime(post.date) }}</div>
+          </q-card-section>
+        </q-card>
+      </div>
 
-        <q-item-section>
-          <q-item-label class="text-bold">MaliciousQ</q-item-label>
-          <q-item-label caption> {{ post.location }} </q-item-label>
-        </q-item-section>
-      </q-item>
-
-      <q-separator />
-
-      <q-img :src="post.imageUrl" />
-
-      <q-card-section>
-        <div>{{ post.caption }}</div>
-        <div class="text-caption text-grey">{{ fancyTime(post.date) }}</div>
-      </q-card-section>
-    </q-card>
+      <div class="col large-screen-only">
+        <q-item class="fixed">
+          <q-item-section avatar>
+            <q-avatar size="xl">
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+            </q-avatar>
+          </q-item-section>
+          <q-item-section>
+            <q-item-label class="text-bold">MaliciousQ</q-item-label>
+            <q-item-label caption> Agent Smith </q-item-label>
+          </q-item-section>
+        </q-item>
+      </div>
+    </div>
   </q-page>
 </template>
 
