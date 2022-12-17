e8887223bc5ab8f75557a43b1b8204eab126ca04
deni <deni@debian>
2022-12-17 04:29:14 -0600

    saving file as blob

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -3,11 +3,17 @@
     <div
       class="camera-frame q-pa-md col-6 col-md-4 row justify-center items-center"
     >
-      <video ref="video" autoplay class="full-width" />
-      <!-- <img class="fit" src="https://cdn.quasar.dev/img/parallax2.jpg" /> -->
+      <video v-show="!isCaptured" ref="video" autoplay class="full-width" />
+      <canvas v-show="isCaptured" ref="canvas" class="full-width" />
     </div>
     <div class="text-center q-pa-md col-12">
-      <q-btn round color="grey-10" size="lg" icon="eva-camera" />
+      <q-btn
+        @click="captureImage"
+        round
+        color="grey-10"
+        size="lg"
+        icon="eva-camera"
+      />
     </div>
     <div class="col-12 col-sm-6 row justify-center q-gutter-md">
       <q-input class="col-12" v-model="caption" label="Caption" dense="dense" />
@@ -29,11 +35,14 @@ export default {
   name: "PageCamera",
   data() {
     return {
-      id: uid(),
-      caption: "",
-      location: "",
-      photo: null,
-      date: Date.now(),
+      post: {
+        id: uid(),
+        caption: "",
+        location: "",
+        photo: null,
+        date: Date.now(),
+      },
+      isCaptured: false,
     };
   },
   methods: {
@@ -53,6 +62,42 @@ export default {
         .then((stream) => {
           this.$refs.video.srcObject = stream;
         });
+    },
+    captureImage() {
+      let video = this.$refs.video;
+      let canvas = this.$refs.canvas;
+
+      canvas.width = video.getBoundingClientRect().width;
+      canvas.height = video.getBoundingClientRect().height;
+
+      let context = canvas.getContext("2d");
+      context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+      this.isCaptured = true;
+      this.post.photo = this.dataURItoBlob(canvas.toDataURL());
+    },
+    dataURItoBlob(dataURI) {
+      // convert base64 to raw binary data held in a string
+      // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+      var byteString = atob(dataURI.split(",")[1]);
+
+      // separate out the mime component
+      var mimeString = dataURI.split(",")[0].split(":")[1].split(";")[0];
+
+      // write the bytes of the string to an ArrayBuffer
+      var ab = new ArrayBuffer(byteString.length);
+
+      // create a view into the buffer
+      var ia = new Uint8Array(ab);
+
+      // set the bytes of the buffer to the correct values
+      for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+      }
+
+      // write the ArrayBuffer to a blob, and you're done
+      var blob = new Blob([ab], { type: mimeString });
+      return blob;
     },
   },
   mounted() {
