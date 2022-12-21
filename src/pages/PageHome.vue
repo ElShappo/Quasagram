3addc63e357f1c111ce3d4932c7fd918163e9794
deni <deni@debian>
2022-12-21 03:03:14 -0600

    added skeleton

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -2,31 +2,65 @@
   <q-page class="constrain q-pa-md">
     <div class="row q-col-gutter-lg">
       <div class="col-12 col-sm-8">
-        <q-card
-          v-for="post in posts"
-          :key="post.id"
-          class="card-post q-mb-md"
-          flat
-          bordered
-        >
-          <q-item>
-            <q-item-section avatar>
-              <q-avatar>
-                <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
-              </q-avatar>
-            </q-item-section>
-            <q-item-section>
-              <q-item-label class="text-bold">MaliciousQ</q-item-label>
-              <q-item-label caption> {{ post.location }} </q-item-label>
-            </q-item-section>
-          </q-item>
-          <q-separator />
-          <q-img :src="post.imageUrl" />
-          <q-card-section>
-            <div>{{ post.caption }}</div>
-            <div class="text-caption text-grey">{{ fancyTime(post.date) }}</div>
-          </q-card-section>
-        </q-card>
+        <template v-if="!loadingPosts">
+          <q-card
+            v-for="post in posts"
+            :key="post.id"
+            class="card-post q-mb-md"
+            flat
+            bordered
+          >
+            <q-item>
+              <q-item-section avatar>
+                <q-avatar>
+                  <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+                </q-avatar>
+              </q-item-section>
+              <q-item-section>
+                <q-item-label class="text-bold">MaliciousQ</q-item-label>
+                <q-item-label caption> {{ post.location }} </q-item-label>
+              </q-item-section>
+            </q-item>
+            <q-separator />
+            <q-img :src="post.imageUrl" />
+            <q-card-section>
+              <div>{{ post.caption }}</div>
+              <div class="text-caption text-grey">
+                {{ fancyTime(post.date) }}
+              </div>
+            </q-card-section>
+          </q-card>
+        </template>
+        <template v-else>
+          <q-card flat bordered size="40px">
+            <q-item>
+              <q-item-section avatar>
+                <q-skeleton type="QAvatar" animation="fade" />
+              </q-item-section>
+
+              <q-item-section>
+                <q-item-label>
+                  <q-skeleton type="text" animation="fade" />
+                </q-item-label>
+                <q-item-label caption>
+                  <q-skeleton type="text" animation="fade" />
+                </q-item-label>
+              </q-item-section>
+            </q-item>
+
+            <q-skeleton height="200px" square animation="fade" />
+
+            <q-card-section>
+              <q-skeleton type="text" class="text-subtitle2" animation="fade" />
+              <q-skeleton
+                type="text"
+                width="50%"
+                class="text-subtitle2"
+                animation="fade"
+              />
+            </q-card-section>
+          </q-card>
+        </template>
       </div>
 
       <div class="col large-screen-only">
@@ -54,29 +88,8 @@ export default {
   name: "PageHome",
   data() {
     return {
-      posts: [
-        // {
-        //   id: 1,
-        //   caption: "Red Square",
-        //   date: 1670261361432,
-        //   location: "Moscow, Russia",
-        //   imageUrl: "https://cdn.quasar.dev/img/parallax2.jpg",
-        // },
-        // {
-        //   id: 2,
-        //   caption: "Red Square",
-        //   date: 1670261361432,
-        //   location: "Moscow, Russia",
-        //   imageUrl: "https://cdn.quasar.dev/img/parallax2.jpg",
-        // },
-        // {
-        //   id: 3,
-        //   caption: "Red Square",
-        //   date: 1670261361432,
-        //   location: "Moscow, Russia",
-        //   imageUrl: "https://cdn.quasar.dev/img/parallax2.jpg",
-        // },
-      ],
+      posts: [],
+      loadingPosts: false,
     };
   },
   methods: {
@@ -85,14 +98,23 @@ export default {
     },
     getPosts() {
       // console.log("Get posts method fired");
+      this.loadingPosts = true;
       axios
         .get("http://192.168.25.129:3000/posts")
         .then((response) => {
           // console.log(response.data);
           this.posts = response.data;
+          this.loadingPosts = false;
         })
         .catch((error) => {
           console.log("error: ", error);
+          this.$q.notify({
+            type: "warning",
+            position: "top",
+            message: "Unable to fetch posts from database",
+            icon: "eva-alert-circle-outline",
+          });
+          this.loadingPosts = false;
         });
     },
   },
