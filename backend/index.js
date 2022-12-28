e97831d36c6e0c7f530014cdf35975158cfb17b6
deni <deni@debian>
2022-12-28 08:01:13 -0600

    admin panel now stable

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,10 @@
 */
 
 let busboy = require("busboy");
+let path = require("path");
+let os = require("os");
+let fs = require("fs");
+let UUID = require("uuid-v4");
 
 const express = require("express");
 const {
@@ -10,6 +14,7 @@ const {
   applicationDefault,
   cert,
 } = require("firebase-admin/app");
+const { getStorage } = require("firebase-admin/storage");
 const {
   getFirestore,
   Timestamp,
@@ -30,9 +35,11 @@ const serviceAccount = require("./quasagram-6b4de-8e066134a44f.json");
 
 initializeApp({
   credential: cert(serviceAccount),
+  storageBucket: "quasagram-6b4de.appspot.com",
 });
 
 const db = getFirestore();
+const bucket = getStorage().bucket();
 
 /*
   endpoint - posts
@@ -63,14 +70,22 @@ app.post("/createPost", (request, response) => {
 
   console.log("POST request");
   const bb = busboy({ headers: request.headers });
+
+  let fields = {};
+  let fileData = {};
+
+  let uuid = UUID();
+
   bb.on("file", (name, file, info) => {
     const { filename, encoding, mimeType } = info;
-    console.log(
-      `File [${name}]: filename: %j, encoding: %j, mimeType: %j`,
-      filename,
-      encoding,
-      mimeType
-    );
+
+    let filepath = path.join(os.tmpdir(), filename);
+    file.pipe(fs.createWriteStream(filepath));
+
+    fileData = { filepath, mimeType };
+
+    console.log("Resulting filepath: ", filepath);
+
     file
       .on("data", (data) => {
         console.log(`File [${name}] got ${data.length} bytes`);
@@ -79,13 +94,44 @@ app.post("/createPost", (request, response) => {
         console.log(`File [${name}] done`);
       });
   });
+
   bb.on("field", (name, val, info) => {
-    console.log(`Field [${name}]: value: %j`, val);
+    fields[name] = val;
   });
+
   bb.on("close", () => {
     console.log("Done parsing form!");
-    response.writeHead(303, { Connection: "close", Location: "/" });
-    response.end();
+    console.log(fields);
+    console.log("Unique id: ", uuid);
+
+    bucket.upload(
+      fileData.filepath,
+      {
+        uploadType: "media",
+        metadata: {
+          metadata: {
+            contentType: fileData.mimeType,
+            firebaseStorageDownloadTokens: uuid,
+          },
+        },
+      },
+      (err, uploadedFile) => {
+        if (!err) {
+          db.collection("posts")
+            .doc(fields.id)
+            .set({
+              id: fields.id,
+              caption: fields.caption,
+              location: fields.location,
+              date: parseInt(fields.date),
+              imageUrl: `https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/${uploadedFile.name}?alt=media&token=${uuid}`,
+            })
+            .then(() =>
+              response.send(`Post with id=${fields.id} successfully added`)
+            );
+        }
+      }
+    );
   });
   request.pipe(bb);
 });
